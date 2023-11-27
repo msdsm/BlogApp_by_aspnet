cdf9c6b1e7e4a1d30cd33816be47f6e83411b694
益田 裕也 <masuda.yuya@boi.jp>
2023-11-27 13:01:27 +0900

    認証機能の実装まで

---
+++ MyBlogApp/Controllers/LoginController.cs
@@ -0,0 +1,50 @@
+﻿using MyBlogApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace MyBlogApp.Controllers
+{
+    [AllowAnonymous]
+    public class LoginController : Controller
+    {
+        readonly private CustomMembershipProvider membershipProvider = new CustomMembershipProvider();
+
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        
+
+        // POST: Login
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index([Bind(Include = "UserName,Password")] LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (this.membershipProvider.ValidateUser(model.UserName, model.Password))
+                {
+                    FormsAuthentication.SetAuthCookie(model.UserName, false);
+                    return RedirectToAction("Index", "Articles");
+                }
+            }
+
+            return View(model);
+        }
+
+        // GET: Login/SignOut
+        public ActionResult SignOut()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Articles");
+        }
+
+        
+    }
+}
